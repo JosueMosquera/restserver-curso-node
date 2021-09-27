40450627b0b11de5b9c66cdc3cc246556076a0ab
JosueMosquera <82737793+JosueMosquera@users.noreply.github.com>
2021-09-27 18:55:51 -0500

    google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcrypt = require("bcryptjs");
 const Usuario = require("../models/usuario");
 const {generarJWT} = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
   try {
@@ -39,6 +40,45 @@ const login = async (req, res = response) => {
     });
   }
 };
+const googleSignIn = async(req,res=response) =>{
+  const{id_token} = req.body;
+  try {
+    const {nombre,img,correo} = await googleVerify(id_token);
+    let usuario= await Usuario.findOne({correo});
+    if(!usuario){
+      // tengo que crearlo
+      const data={
+        nombre:nombre,
+        correo:correo,
+        password:'es parte del metodo de google',
+        img:img,
+        rol:'USER_ROLE',
+        google:true
+      }
+      usuario=new Usuario(data);
+      await usuario.save();
+    }
+    //si el usuario en DB tiene estado false en google
+    if(!usuario.estado){
+      return res.status(401).json({
+        msg:'hable con el admin, usuario bloqueado -estado false'
+      })
+    }
+    const token = await generarJWT(usuario.id);
+    res.json({
+      usuario,
+      token
+    })
+    
+  } catch (error) {
+    res.status(400).json({
+      ok:false,
+      msg:'el token no se pudo verificar'
+    })
+  }
+
+}
 module.exports = {
   login,
+  googleSignIn
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const {login} = require('../controllers/auth');
+const {login, googleSignIn} = require('../controllers/auth');
 const { validarCampos } = require("../middlewares/validar-campos");
 const router = Router();
 router.post('/login',[
@@ -8,5 +8,9 @@ router.post('/login',[
     check('password','la contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ],login);
+router.post('/google',[
+    check('id_token','El id token de google es necesario').not().isEmpty(),
+    validarCampos
+],googleSignIn);
 
 module.exports=router;
